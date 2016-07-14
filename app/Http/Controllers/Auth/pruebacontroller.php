5959af384a79a1f5a6c8b2a0fec17c0040bc336f
Victor Jose Caraballo Bellorin <victorjcb28@gmail.com>
2016-07-13 22:19:29 -0400

    Add files via upload

---
+++ app/Http/Controllers/Auth/pruebacontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cinema\Http\Controllers\Auth;
+
+
+
+class PruebaController extends Controller
+{
+
+        public function index(){
+          return "hola";
+        }
+
+}
---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cinema\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Cinema\Http\Requests;
+use Cinema\Http\Controllers\Controller;
+
+class FrontController extends Controller
+{
+  public function __construct(){
+    $this->middleware('auth',['only'=>'admin']);
+  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     public function contacto()
+     {
+        return view('contacto');
+
+     }
+
+     public function reviews()
+     {
+        return view('reviews');
+     }
+
+     public function admin(){
+          return view('admin.index');
+     }
+}
